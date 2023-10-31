9be28a1dacdea28b936f76287b391cadac79f5fb
lsankar4033 <lsankar4033@gmail.com>
2023-10-31 13:39:16 -0700

    feat: add pg members+donors

--- src/eth-og.ts
+++ src/eth-og.ts
@@ -1,7 +1,11 @@
 import { readCSV } from './utils';
 
+let presaleAddrs: { [key: string]: string }[] = [];
+
 const isPresale = async (address: string): Promise<boolean> => {
-  const presaleAddrs = await readCSV('data/eth-presale.csv');
+  if (presaleAddrs.length === 0) {
+    presaleAddrs = await readCSV('data/eth-presale.csv');
+  }
 
   return presaleAddrs
     .map((k) => k['address'])
@@ -9,8 +13,12 @@ const isPresale = async (address: string): Promise<boolean> => {
     .includes(address.toLowerCase());
 };
 
+let beaconGenesisAddrs: { [key: string]: string }[] = [];
+
 const isBeaconGenesis = async (address: string): Promise<boolean> => {
-  const beaconGenesisAddrs = await readCSV('data/beacon-genesis.csv');
+  if (beaconGenesisAddrs.length === 0) {
+    beaconGenesisAddrs = await readCSV('data/beacon-genesis.csv');
+  }
 
   return beaconGenesisAddrs
     .map((k) => k['depositor'])
@@ -18,16 +26,68 @@ const isBeaconGenesis = async (address: string): Promise<boolean> => {
     .includes(address.toLowerCase());
 };
 
+const normAddrFromPgDune = (address: string): string => {
+  const addrRE = /(\b0x[a-f0-9]{40}\b)/;
+
+  const match = address.match(addrRE);
+  if (match) {
+    return match[0].toLowerCase();
+  }
+
+  return '';
+};
+
+let pgMembers: { [key: string]: string }[] = [];
+
+const isPGMember = async (address: string): Promise<boolean> => {
+  if (pgMembers.length === 0) {
+    pgMembers = await readCSV('data/pg-members.csv');
+  }
+  return pgMembers
+    .map((k) => k['member'])
+    .map(normAddrFromPgDune)
+    .includes(address.toLowerCase());
+};
+
+let pgDonors: { [key: string]: string }[] = [];
+
+const isPGDonor = async (address: string): Promise<boolean> => {
+  if (pgDonors.length === 0) {
+    pgDonors = await readCSV('data/pg-donors.csv');
+  }
+
+  return pgDonors
+    .map((k) => k['donor_address'])
+    .map(normAddrFromPgDune)
+    .includes(address.toLowerCase());
+};
+
 export const getEthTraits = async (address: string): Promise<string[]> => {
+  let traits = [];
+
   const _isPresale = await isPresale(address);
   if (_isPresale) {
     console.log(`Address ${address} is a presale address`);
+    traits.push('eth:presale');
   }
 
   const _isBeaconGenesis = await isBeaconGenesis(address);
   if (_isBeaconGenesis) {
     console.log(`Address ${address} is a beacon genesis address`);
+    traits.push('eth:beacon-genesis');
   }
 
-  return [];
+  const _isPGMember = await isPGMember(address);
+  if (_isPGMember) {
+    console.log(`Address ${address} is a PG member`);
+    traits.push('eth:pg-member');
+  }
+
+  const _isPGDonor = await isPGDonor(address);
+  if (_isPGDonor) {
+    console.log(`Address ${address} is a PG donor`);
+    traits.push('eth:pg-donor');
+  }
+
+  return traits;
 };
